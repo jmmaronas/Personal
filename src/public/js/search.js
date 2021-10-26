d9552e58fdca6ae287e9cdc61cb47ec30fccd7ee
Juan Manuel <jmmaronas@gmail.com>
2021-10-26 14:48:37 -0300

    pruebas
    pruebas en la busqueda

--- src/public/js/search.js
+++ src/public/js/search.js
@@ -1,24 +1,37 @@
-let respuesta;
+
 fetch('/api')
   .then(response => response.json())
   .then(data =>{
-    respuesta=  data.personal;
-    console.log(respuesta);
+    respuesta=  data.personal;    
     let inputResult = document.querySelector("#search")
-    inputResult.addEventListener("keydown", e=>{       
-      let result=[];
-      console.log(inputResult.value);      
-      for(element of respuesta){
-        let nombre= element.nombre.toLowerCase();
+    inputResult.addEventListener("keyup", e=>{                   
+      let count=0;
+      document.getElementById("listadoPersonal").innerHTML="";
+      for(e of respuesta){
+        let nombre= e.nombre.toLowerCase();
         if(nombre.indexOf(inputResult.value)!=-1){          
-          result.push(element);
-          console.log(result);          
+          count+=1;          
+          document.getElementById("listadoPersonal").innerHTML+=`
+              <tr class="bg-${e.state ? 'light' : 'warning'}">
+                <td>${count}</td>
+                <td>${e.nombre}</td>
+                <td>${e.legajo}</td>
+                <td>${e.dni}</td>
+                <td>${e.especialidad}</td>
+                <td>${e.turno} </td>
+                <td>${e.horario}</td>
+              
+                <td>
+                    <a href='/status/${e._id}' 
+                        class="btn ${e.state ? 'btn-success' : 'btn-secondary'}"
+                        ><i class="${e.state ? 'far fa-check-circle' : 'far fa-times-circle'}"></i></a>
+                    <a href='/delete/${e._id}' class="btn btn-danger"><i class="far fa-trash-alt"></i></a>
+                    <a href='/edit/${e._id}' class="btn btn-primary"><i class="fas fa-user-edit"></i></a>
+                </td>
+              </tr>              
+          `;
         }
-      }
-      // for(elem of result){
-      //   document.querySelector("#listSearch").innerHTML+=`
-      //     <li>${result.nombre}</li>`
-      //}      
+      }      
     });
   });
 
